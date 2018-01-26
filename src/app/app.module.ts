3f9698434d14d46a75cce933f8c002a5664018a9
Amy Churchwell <amy.churchwell@gmail.com>
2018-01-26 10:33:11 -0800

    Add firebase config info to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,6 +12,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ListingComponent } from './listing/listing.component';
 import { AddListingComponent } from './add-listing/add-listing.component';
 import { EditListingComponent } from './edit-listing/edit-listing.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDphcAgYGisiCGamS5_2XiYbPp7p0Z3IXI",
+    authDomain: "study-35305.firebaseapp.com",
+    databaseURL: "https://study-35305.firebaseio.com",
+    projectId: "study-35305",
+    storageBucket: "study-35305.appspot.com",
+    messagingSenderId: "850071496750"
+};
 
 const appRoutes: Routes = [
   {path:'', component: HomeComponent},
